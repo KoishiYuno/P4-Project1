9042d587ee38653ba268be53975570566e1cba2f
KoishiYuno <gzdhy8895@gmail.com>
2019-05-06 14:21:09 +0800

    Add files via upload

---
+++ Assets/StartGame.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartGame : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    private void OnGUI()
+    {
+        GUI.Label(new Rect((Screen.width / 2) - 40, 50, 80, 30), "Welcome");
+        if (GUI.Button(new Rect(Screen.width / 2 -100, 250, 200, 30), "Start a New Game"))
+        {
+            Application.LoadLevel(0);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
